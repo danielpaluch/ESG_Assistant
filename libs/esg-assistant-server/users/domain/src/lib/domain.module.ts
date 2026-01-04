413e3a577fe5054096ae72db7e3c61ec66d7bf55
danielpaluch <danielpaluch37@gmail.com>
2026-01-04 17:43:21 +0100

    chore: removed unnecessary provider in user domain

--- libs/esg-assistant-server/users/domain/src/lib/domain.module.ts
+++ libs/esg-assistant-server/users/domain/src/lib/domain.module.ts
@@ -1,16 +1,9 @@
-import { USER_REPOSITORY } from './ports/user-repository.port';
 import { Module } from '@nestjs/common';
-import { UserMongooseRepository } from '@esg-assistant-server/users/infrastructure';
 
 @Module({
   controllers: [],
-  providers: [
-    {
-      provide: USER_REPOSITORY,
-      useClass: UserMongooseRepository,
-    },
-  ],
+  providers: [],
   imports: [],
-  exports: [USER_REPOSITORY],
+  exports: [],
 })
 export class UserDomainModule {}
